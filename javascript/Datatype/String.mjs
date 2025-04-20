a01a4e371b167c065cb0db541347bc27da077e6a
Santosh Dahal <thesaantdahal@gmail.com>
2025-04-20 08:47:00 +0545

    javascript basics

---
+++ javascript/Datatype/String.mjs
@@ -0,0 +1,15 @@
+//let name ="Ram"
+// Single line comment : ctrl + /
+// Multi line comment : Alt + shift + A
+//console.log("Ram");
+// 
+
+let name = "Sham";
+name = "Shyam";
+console.log(name);
+
+/* const name1 = "Sham";
+name1 = "Shyam1"; // Error: Assignment to constant variable.
+
+// This will throw an error because name1 is a constant and cannot be reassigned.
+console.log(name1); */ // This will throw an error because name1 is a constant and cannot be reassigned.
---
+++ javascript/Datatype/operator.mjs
@@ -0,0 +1,6 @@
+// Operators in javascript
+// Operators are special symbols that perform operations on variables and values.
+// Operators are used to assign values, compare values, perform arithmetic operations, and more.
+
+// Arithmetic Operators
+// +, - , * , / , % 
