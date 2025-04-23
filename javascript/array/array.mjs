669921b1d15c337446f28d373cf04cd73f111f6e
Santosh Dahal <thesaantdahal@gmail.com>
2025-04-23 19:17:19 +0545

    feat

--- javascript/array/array.mjs
+++ javascript/array/array.mjs
@@ -1,13 +1,26 @@
-// Array in javascript
-// Array is a data structure that can hold multiple values
+//? Array in javascript
+//? Array is a data structure that can hold multiple values
 
-// syntax of array
-// let arrayName = [value1, value2, value3, ...];
+//? syntax of array
+//? let arrayName = [value1, value2, value3, ...];
 
 let list = ["Shyam", "Ram", 5, "Gita"];
 console.log(list);
 
-// List in javascript is a collection of items that can be of different types
-// Example: list of names, list of numbers, list of objects, list of arrays, etc.
+//? List in javascript is a collection of items that can be of different types
+//? Example: list of names, list of numbers, list of objects, list of arrays, etc.
 
 // Syntax of Map
+//? Differences between .js and .mjs is that .mjs is a module file and .js is a script file
+//? .mjs file can be imported in .js file but .js file cannot be imported in .mjs file
+//? .mjs file can be used to create modules that can be imported in other .mjs files
+const obj1 = {
+  name: "Developer",
+};
+
+const obj2 = {
+  name: "Developer",
+};
+console.log(obj1 === obj2); // false because they are different objects in memory
+console.log(obj1 == obj2); // false because they are different objects in memory
+// Non prqimitive data types are compared by reference and primitive data types are compared by value
