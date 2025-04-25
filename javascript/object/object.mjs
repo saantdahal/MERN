5f2ede0385784a93e2023495050cddd25f8b0d1b
Santosh Dahal <thesaantdahal@gmail.com>
2025-04-25 08:04:17 +0545

    feat: objects

--- javascript/object/object.mjs
+++ javascript/object/object.mjs
@@ -33,7 +33,7 @@ class Person {
 // Creating an object of the Person class
 let person1 = new Person("Shyam", 25);
 person1.display(); */
-let info = {
+/* let info = {
   name: "Shyam",
   age: 25,
   score: 90,
@@ -41,3 +41,79 @@ let info = {
 };
 
 console.log(info.name.toUpperCase()); // Harry because it will override the previous value of name. Duplicate keys are not allowed in object
+ */
+
+/* let info = {
+  fullName: "Shthyan",
+  address: "Kathmandu",
+  favFruit: ["Apple", "Banana", "Mango"],
+  location: {
+    location: "Kathmandu",
+    country: {
+      name: "Nepal",
+      code: "NP",
+    },
+  },
+  address: () => {
+    console.log("I am function");
+  },
+};
+
+console.log(info.favFruit[0]);
+info.address(); */
+
+// This keyword is not used in arrow function. It is used in nirml function.
+/* let info = {
+  firstName: "Shyam",
+  lastName: "Shrestha",
+  fullName: function () {
+    return this.firstName + " " + this.lastName;
+  },
+
+  fullName: () => {
+    // make it work with arrow function
+    console.log(this?.firstName + " " + this?.lastName); // undefined because arrow function does not have its own this keyword
+  },
+};
+
+console.log(info.fullName()); */
+
+//Find the total sum of all elements in the array of objects
+
+/* let info = [
+  {
+    name: "earphone",
+    price: 100,
+  },
+  {
+    name: "phoone",
+    price: 1000,
+  },
+  {
+    name: "laptop",
+    price: 10000,
+  },
+];
+// Find the total sum of all elements in the array of objects
+
+let totalSumm = info.map((a) => a.name).reduce((p, c) => p + c, 0);
+console.log(totalSumm); */
+
+/* // convert object into array
+let info = {
+  name: "Ram",
+  age: 50,
+  address: "Kathmandu",
+};
+
+console.log(Object.keys(info));
+console.log(Object.values(info)); */
+
+// Convert Array into Object
+
+let info = {
+  name: "Ram",
+  age: 50,
+  address: "Kathmandu",
+};
+console.log(Object.entries(info)[2]);
