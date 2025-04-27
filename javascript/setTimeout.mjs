302fac47daf4a5f67060fb8b2fcf68b2135ee4a2
Santosh Dahal <thesaantdahal@gmail.com>
2025-04-27 08:47:14 +0545

    feat: settimeout

---
+++ javascript/setTimeout.mjs
@@ -0,0 +1,7 @@
+// setTimeout
+// setTimeout is a function that executes a function after a specified number of milliseconds
+
+// Syntax: setTimeout(function, delayTime)
+setTimeout(() => {
+  console.log("Hello World!");
+}, 5000);
