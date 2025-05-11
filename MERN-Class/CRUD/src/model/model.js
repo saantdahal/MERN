1fc3c42fbe1c5884f218a962faf12fb97ba786e5
Santosh Dahal <thesaantdahal@gmail.com>
2025-05-11 18:08:45 +0545

    feat: init

--- MERN-Class/CRUD/src/model/model.js
+++ MERN-Class/CRUD/src/model/model.js
@@ -1,5 +1,7 @@
 // This is a model file that connects to the database and exports the models
 // It uses mongoose to create a connection to the database and defines the schema for the models
+// This is a model file that connects to the database and exports the models
+// It uses mongoose to create a connection to the database and defines the schema for the models
 import mongoose from "mongoose";
 import studentSchema from "../schema/studentSchema.js";
 import webUserSchema from "../schema/webUserSchema.js";
