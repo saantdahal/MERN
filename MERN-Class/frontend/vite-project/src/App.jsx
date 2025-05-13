37c3e46736a3d589d044a250ead3557fef011f96
Santosh Dahal <thesaantdahal@gmail.com>
2025-05-13 09:02:00 +0545

    feat: frontend setup

---
+++ MERN-Class/frontend/vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import "../global.css";
+
+function App() {
+  // Here we write javascript code
+  return (
+    <>
+      {/* React Fragment */}
+      <h1 style={{ color: "indigo" }}>
+        This is my first REACT app with Vite and MERN stack
+      </h1>
+      <h2 className="firstcss">This is my h2</h2>
+    </>
+  );
+}
+
+export default App;
