0f2f3ab69e207b079cdf61e1bcb43e6aa01c3a2c
Santosh Dahal <thesaantdahal@gmail.com>
2025-06-01 11:11:28 +0545

    feat: url changed for DB

--- MERN-Class/CRUD/src/connectDB/connectionDatabase.js
+++ MERN-Class/CRUD/src/connectDB/connectionDatabase.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 const connectDatabase = () => {
   mongoose.connect(
-    "mongodb+srv://curd-practice:aPbre4Pt3od1Zqx2@cluster0.tobhffg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    "mongodb+srv://curd-practice:qCqinLZLoibBmoUR@cluster0.tobhffg.mongodb.net/"
   );
   console.log("MongoDB connected successfully");
 };
